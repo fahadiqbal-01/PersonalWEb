ef5a111551234628211f64dd41c67cc7263b5e38
FahadIqbal-01 <fahad.iqbal.im.47@gmail.com>
2024-06-23 23:57:16 +0600

    ntg

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Components/Banner";
 import Container from "../Components/Container";
+import Some from "./Some";
 
 const Home = () => {
   return (
     <section className=" mt-[50px] pb-[120px] ">
       <Container>
         <Banner />
+        <Some />
       </Container>
     </section>
   );
--- src/Pages/something.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const something = () => {
-  return (
-    <div>something</div>
-  )
-}
-
-export default something
